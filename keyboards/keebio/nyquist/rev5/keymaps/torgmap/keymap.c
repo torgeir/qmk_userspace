248b1044e8fcaaa7231248c432d2ad26c4e22b30
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2026-09-10 22:17:37 +0200

    fix whitespace

--- keyboards/keebio/nyquist/rev5/keymaps/torgmap/keymap.c
+++ keyboards/keebio/nyquist/rev5/keymaps/torgmap/keymap.c
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_EQL,      KC_1,               KC_2,                KC_3,                KC_4,   KC_5,        /**/ KC_6,         KC_7,        KC_8, KC_9,   KC_0,         KC_LBRC/*å*/,
   S(KC_BSPC),  KC_F1,              KC_F2,               KC_F3,               KC_F4,  KC_PERC,     /**/ NO_SLSH/*/*/, KC_4,        KC_5, KC_6,   KC_PAST/***/, KC_PEQL/*=*/,
   KC_LSFT,     KC_F5,              KC_F6,               KC_F7,               KC_F8,  _______,     /**/ KC_PPLS/*+*/, KC_1,        KC_2, KC_3,   KC_PMNS/*-*/, KC_BSLS/*@*/,
-  TO(_QWERTY), MT(KC_LCTL, KC_F9), MT(KC_LALT, KC_F10), MT(KC_LGUI, KC_F11), KC_F12, TO(_QWERTY), /**/ OSL(_ONEHAND),      TO(_QWERTY), KC_0, KC_DOT, KC_COMM,      TO(_NAV)
+  TO(_QWERTY), MT(KC_LCTL, KC_F9), MT(KC_LALT, KC_F10), MT(KC_LGUI, KC_F11), KC_F12, TO(_QWERTY), /**/ OSL(_ONEHAND),TO(_QWERTY), KC_0, KC_DOT, KC_COMM,      TO(_NAV)
 ),
 [_ADJUST] = LAYOUT_ortho_4x12(
   _______,  _______,  MS_WHLD, MS_UP,   MS_WHLU, _______, /**/ _______, KC_PGDN, KC_PGUP, KC_MPRV, KC_MPLY, KC_MNXT,
